c239d16d7328ace7beb1db2328132ff3aa99a141
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-04-04 22:01:27 +0200

    added BurgerIngredient

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './BurgerBuilder.css';
+const burgerIngredient = (props) => {
+  let ingredient = null;
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className='BreadBottom'></div>;
+      break;
+    case ('bread-top'):
+      ingredient = (
+        <div className={'BreadTop'}>
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredient = <div className='Meat'></div>;
+      break;
+    case ('cheese'):
+      ingredient = <div className='Cheese'></div>;
+      break;
+    case ('salad'):
+      ingredient = <div className='Salad'></div>;
+      break;
+    case ('bacon'):
+      ingredient = <div className='Bacon'></div>;
+      break;
+    default:
+      ingredient = null;
+  }
+  return (
+    ingredient
+  )
+}
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+}
+export default burgerIngredient;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
-
 class BurgerBuilder extends Component {
   constructor(props) {
     super(props);
