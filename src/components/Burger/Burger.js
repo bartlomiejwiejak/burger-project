5f7ef093f656304028e9a5b17cc0778fcf1fa5ba
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-04-05 19:53:20 +0200

    passing ingredients through props to Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
-const burger = () => {
+const burger = (props) => {
+  let ingredientComponents = [];
+  for (const ingredient in props.ingredients) {
+    for (let i = 0; i < props.ingredients[ingredient]; i++) {
+      ingredientComponents.push(<BurgerIngredient type={ingredient} key={ingredient + i} />);
+    }
+  }
   return (
     <div className='Burger'>
       <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='cheeese' />
+      {ingredientComponents}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
 }
+
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
   }
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Burger controls</div>
       </Aux>
     );
