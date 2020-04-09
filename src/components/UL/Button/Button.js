d6cf1ab17cb9d3cf00d47a5161c5280acc1dcda6
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-04-09 04:12:19 +0200

    Button.js component

---
+++ src/components/UL/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) => {
+  return (
+    <button onClick={props.click} className={['Button', props.btnType].join('')}>{props.children}</button>
+  );
+}
+
+export default Button;
