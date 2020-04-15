9ffc9bf310e54b641371b28de2440a712923c189
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-04-15 05:42:51 +0200

    moved layout to hoc folder

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import './Layout.scss';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
   state = {
     SideDrawerShown: false
