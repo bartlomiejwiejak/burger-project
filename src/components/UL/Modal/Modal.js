b32598cef58c562a038b61544a9fa7e31bf9e4ea
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-04-15 05:35:57 +0200

    Added shouldComponentUpdate in modal

--- src/components/UL/Modal/Modal.js
+++ src/components/UL/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop cancel={props.cancelPurchase} show={props.show} />>
-      <div className='Modal'
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-      </div>
-    </Aux>
-  );
+class Modal extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  }
+  componentDidUpdate() {
+    console.log("Update modaluuu")
+  }
+  render() {
+    return (
+      <Aux>
+        <Backdrop cancel={this.props.cancelPurchase} show={this.props.show} />
+        <div className='Modal'
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
 }
 
 export default Modal;
