63d9e78cf2d18bb3c3b925928777428b19c268ee
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-05-06 19:04:26 +0200

    added checkout page

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Burger from '../../Burger/Burger';
+import Button from '../../UL/Button/Button';
+import './CheckoutSummary.css';
+const CheckoutSummary = (props) => {
+  return (
+    <div className='CheckoutSummary'>
+      <h1>We hope it tastes well!</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button click btnType='Success'>Continue</Button>
+      <Button click btnType='Danger'>Cancel</Button>
+    </div>
+  );
+}
+
+export default CheckoutSummary;
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1,
+      bacon: 1
+    }
+  }
+  render() {
+    return (
+      <CheckoutSummary ingredients={this.state.ingredients} />
+    );
+  }
+}
+
+export default Checkout;
