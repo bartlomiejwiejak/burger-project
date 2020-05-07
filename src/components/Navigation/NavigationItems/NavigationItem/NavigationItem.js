d1c2e766dd708a54c27a13630db0f0a51cdb200d
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-05-07 21:22:37 +0200

    Nav links for /orders and home page

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './NavigationItem.css';
-
+import { NavLink } from 'react-router-dom';
 const NavigationItem = (props) => {
   return (
     <li className='NavigationItem'>
-      <a href={props.link} className={props.active ? 'active' : null}>{props.children}</a>
+      <NavLink exact={props.exact} to={props.link}>{props.children}</NavLink>
     </li>
   );
 }
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const NavigationItems = () => {
   return (
     <ul className='NavigationItems'>
-      <NavigationItem link='/' active>Burger builder</NavigationItem>
-      <NavigationItem link='/'>Checkout</NavigationItem>
+      <NavigationItem link='/' exact>Burger builder</NavigationItem>
+      <NavigationItem link='/orders'>Orders</NavigationItem>
     </ul>
   );
 }
