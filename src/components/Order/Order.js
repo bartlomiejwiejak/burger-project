077c7f15c166a52a3e54fd12dfc69cfc6b0f77db
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-05-07 21:06:54 +0200

    Orders.js and Order.js

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Order.css';
+const Order = () => {
+  return (
+    <div className='Order'>
+      <p>Ingredients: Salad (1)</p>
+      <p>Price: <strong>USD 5.45</strong></p>
+    </div>
+  );
+}
+
+export default Order;
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Order from '../../components/Order/Order';
+class Orders extends Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        <Order />
+        <Order />
+      </div>
+    );
+  }
+}
+
+export default Orders;
