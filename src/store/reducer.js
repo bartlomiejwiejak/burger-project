ff6ccefd8fb0dfcd2a0e868222dabc6f1e8722d6
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-05-16 04:59:05 +0200

    Reducer for ingredient

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,35 @@
 import * as actionTypes from './actions';
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+
+  },
   totalPrice: 0
 }
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case (actionTypes.ADD_INGREDIENT):
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      }
+    case (actionTypes.REMOVE_INGREDIENT):
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      }
+    default:
+      return state;
+  }
 }
-
 export default reducer;
