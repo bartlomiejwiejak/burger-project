aa0c1726c62a189f0286577c2e83112d22c282f0
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-06-01 16:12:59 +0200

    Spinner center

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -49,7 +49,7 @@ class BurgerBuilder extends Component {
     let orderSummary = null;
     let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />
 
-    if (this.props.ings) {
+    if (0) {
       burger = (
         <Aux>
           <Burger ingredients={this.props.ings} />
