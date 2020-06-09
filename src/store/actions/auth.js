6e8c17aaaab66f205465911403ae869eee74d53b
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-06-09 21:38:00 +0200

    Signing in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-
+import axios from 'axios';
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START
@@ -7,19 +7,33 @@ export const authStart = () => {
 }
 export const authFail = (error) => {
   return {
-    type: actionTypes.AUTH_START,
+    type: actionTypes.AUTH_FAIL,
     error: error
   }
 }
 export const authSuccess = (authData) => {
   return {
-    type: actionTypes.AUTH_START,
+    type: actionTypes.AUTH_SUCCESS,
     authData
   }
 }
 
 export const auth = (email, password) => {
   return dispatch => {
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
     dispatch(authStart());
+    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXM6PTyBjnTzL_c3qGhM7MMiOoQqHE2Js', authData)
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data))
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err))
+      })
   }
 }
