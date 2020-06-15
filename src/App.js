9eecabe44fcabae410c9196730c4e8e74935abc9
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-06-16 00:31:25 +0200

    Guarding routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -13,16 +13,29 @@ class App extends Component {
     this.props.onTryAutoSignUp()
   }
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/' exact component={BurgerBuilder} />
+        <Route path='/auth' component={Auth} />
+        <Redirect to='/' />
+      </Switch>
+    )
+    if (this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path='/' exact component={BurgerBuilder} />
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/auth' component={Auth} />
+          <Route path='/logout' component={Logout} />
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path='/' exact component={BurgerBuilder} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/auth' component={Auth} />
-            <Route path='/logout' component={Logout} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -35,4 +48,10 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
