41a8cfa365ae6d028d0127e8858ac68f25d06a79
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-06-16 20:05:25 +0200

    Added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { connect } from 'react-redux';
 import * as actions from './store/actions';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout')
+})
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders')
+})
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth')
+})
+
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignUp()
@@ -16,7 +25,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route path='/' exact component={BurgerBuilder} />
-        <Route path='/auth' component={Auth} />
+        <Route path='/auth' component={asyncAuth} />
         <Redirect to='/' />
       </Switch>
     )
@@ -24,9 +33,9 @@ class App extends Component {
       routes = (
         <Switch>
           <Route path='/' exact component={BurgerBuilder} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
+          <Route path='/auth' component={asyncAuth} />
           <Route path='/logout' component={Logout} />
           <Redirect to='/' />
         </Switch>
