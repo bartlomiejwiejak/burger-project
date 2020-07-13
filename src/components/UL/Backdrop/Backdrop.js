6af1eb699fd987858ce7b9612e4f8cdba8e691f5
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-07-13 06:11:46 +0200

    mobile backdrop fix

--- src/components/UL/Backdrop/Backdrop.js
+++ src/components/UL/Backdrop/Backdrop.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Backdrop.css';
+import './backdrop.scss';
 
 const Backdrop = (props) => {
   return (
-    <div ref={props.reference} onClick={props.isAnimating ? null : props.cancel} className={`Backdrop ${props.isMobile ? 'isMobile' : ''}`}></div>
+    <div ref={props.reference} onClick={props.isAnimating ? null : props.cancel} className={`backdrop ${props.isMobile ? 'isMobile' : ''}`}></div>
   );
 }
 
